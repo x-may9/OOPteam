159dedd9341a4030aa78230b30c728c4f787b5a1
peppercndy <peppercndy@gmail.com>
2024-09-12 20:15:04 +0800

    大致框架完成，可以开始干活

---
+++ GenshinImpact.cpp
@@ -0,0 +1,5 @@
+#include "LinkList.h"
+#include "GenshinImpact.h"
+#include<iostream>
+
+using namespace std;
---
+++ GenshinImpact.h
@@ -0,0 +1,32 @@
+#ifndef GENSHINIMPACT_H
+#define GENSHINIMPACT_H
+#include "LinkList.h"
+
+class Hero
+{
+    private:
+        string name;
+        int stoneNum;
+
+
+    /*根据OOP研讨1.pdf功能所描述的需求，编写7个函数，
+        可以参考Lab3Banking.cpp ，
+        可以使用LinkList.h里面的函数*/
+    public:
+    //开始编写
+};
+
+class Account
+{
+    private:
+        int UID;
+        LinkList<Hero> link;
+
+    /*根据OOP研讨1.pdf功能所描述的需求，编写7个函数，
+        可以参考Lab3Banking.cpp ，
+        可以使用LinkList.h里面的函数*/
+    public:
+    //开始编写
+
+};
+#endif
